27ec77d9cb371e19d721eee0fcebdc8ced35c2ae
Hemang Nakarani <45796208+HemangNakarani@users.noreply.github.com>
2020-01-08 19:02:45 +0530

    Update Config.java

--- app/src/main/java/com/hemangnh18/accomodation/POJO/Config.java
+++ app/src/main/java/com/hemangnh18/accomodation/POJO/Config.java
@@ -2,5 +2,5 @@ package com.hemangnh18.accomodation.POJO;
 
 public class Config {
     public static final String EMAIL ="synapse2020.events@gmail.com";
-    public static final String PASSWORD ="Synapse.Events.2020";
+    public static final String PASSWORD ="______APNA PASSWORD AUR EMAIL ID DAALO______";
 }
